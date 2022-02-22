537e696f77298f246c316031420a0f0eeb5c6d37
soumyah1512 <99666938+soumyah1512@users.noreply.github.com>
2022-02-22 21:26:01 +0530

    Made the Create Account page

---
+++ create_account_page/create_account.js
@@ -0,0 +1,34 @@
+var userData = JSON.parse(localStorage.getItem("signupData")) || [];
+document.getElementById("form").addEventListener("submit", function(event){
+    event.preventDefault();
+    if(document.getElementById("firstName").value == "" || document.getElementById("lastName").value == "" || document.getElementById("email").value == "" || document.getElementById("password").value == "")
+    {
+        // For when user enters incomplete information
+        alert("Please enter full information.");
+        return;
+    }
+    else{
+        // When all the fields are filled in the form
+        var first_name = document.getElementById("firstName").value;
+        var last_name = document.getElementById("lastName").value;
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+        // Creating a object with all the fields data
+        var userInfoObj = {
+            firstname: first_name,
+            lastname: last_name,
+            email: email,
+            pass: password
+        }  
+        // Pushing the object in an array
+        userData.push(userInfoObj);
+        // Creating a key in local storage and storing the above array into it
+        localStorage.setItem("signupData", JSON.stringify(userData));
+
+        // Clearing the fields after taking the data
+        document.getElementById("firstName").value = "";
+        document.getElementById("lastName").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+    }   
+})
