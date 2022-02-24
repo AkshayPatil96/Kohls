0559923533db158fb766457037ad905b4b456e21
Akshay Patil <53942517+AkshayPatil96@users.noreply.github.com>
2022-02-24 15:30:05 +0530

    Done With womens page and completed, homepage, men and women section

--- Sign up/signin.js
+++ Sign up/signin.js
@@ -23,3 +23,6 @@ document.querySelector("#form").addEventListener("submit",data)
             }
         }
     }
+
+
+    
---
+++ Womens Section/women.js
@@ -0,0 +1,559 @@
+var womensdata = [
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5185560_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: " 36.00",
+    originalprice: " 60.00",
+    name: "Women's FLX Affirmation Velvet-Trim Mockneck Top",
+    gender: "Women",
+    size: "m",
+    brand: "Nike",
+    category: "Women Tops",
+    id: 01,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/4975137_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "37.50",
+    originalprice: "50.00",
+    name: "Women's adidas Essentials Logo Fleece Hoodie",
+    gender: "Women",
+    size: "Large",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 02,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5164229_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "45.00",
+    originalprice: "60.00",
+    name: "Plus Size adidas Floral Graphic Hoodie",
+    gender: "Women",
+    size: "m",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 03,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/4935302_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "33.75",
+    originalprice: "45.00",
+    name: "Women's adidas Essential 3-Stripe Fleece Sweatshirt",
+    gender: "Women",
+    size: "m",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 04,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5199404_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "38.50",
+    originalprice: "55.00",
+    name: "Women's adidas Essentials Fleece Full-Zip Hoodie",
+    gender: "Women",
+    size: "xs",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 05,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5164348_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "33.75",
+    originalprice: "45.00",
+    name: "Plus Size adidas Essentials 3-Stripe Fleece Sweatshirt",
+    gender: "Women",
+    size: "Large",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 06,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/4347184_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "16.50",
+    originalprice: "55.00",
+    name: "Women's adidas Performance Polo",
+    gender: "Women",
+    size: "m",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 07,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/3719925_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "$25.00",
+    originalprice: "45.00",
+    name: "Women's adidas Badge of Sport Tee",
+    gender: "Women",
+    size: "xs",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 08,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5145830_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "11.20",
+    originalprice: "28.00",
+    name: "Plus Size adidas x Zoe Saldana Collection Cropped Tee",
+    gender: "Women",
+    size: "m",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 09,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5145817_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "25.00",
+    originalprice: "39.00",
+    name: "Women's adidas Winners 2.0 Tee",
+    gender: "Women",
+    size: "Large",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 10,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5145817_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "14.00",
+    originalprice: "28.00",
+    name: "Women's adidas x Zoe Saldana Collection Crop Tee",
+    gender: "Women",
+    size: "xs",
+    brand: "Adidas",
+    category: "Women Tops",
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/3663888_Power_Red?wid=240&hei=240&op_sharpen=1",
+    saleprice: "35.00",
+    originalprice: "42.00",
+    name: "Women's adidas Hilo Long Sleeve Volleyball Jersey",
+    gender: "Women",
+    size: "m",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 11,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/4986164_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "25.00",
+    originalprice: "36.00",
+    name: "Women's adidas Floral Logo Graphic Tee",
+    gender: "Women",
+    size: "Large",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 12,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/4627327_Black_White?wid=600&hei=600&op_sharpen=1",
+    saleprice: "17.50",
+    originalprice: "25.00",
+    name: "Women's adidas Linear Logo Tee",
+    gender: "Women",
+    size: "xl",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 13,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5186406_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "15.00",
+    originalprice: "30.00",
+    name: "Women's adidas x Zoe Saldana Tee",
+    gender: "Women",
+    size: "s",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 13,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5002369_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "40.00",
+    originalprice: "52.00",
+    name: "Women's adidas Winners 2.0 Tee",
+    gender: "Women",
+    size: "m",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 14,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5002357_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "17.50",
+    originalprice: "35.00",
+    name: "Women's adidas Tiro Pride Jersey",
+    gender: "Women",
+    size: "Large",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 15,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/4981891_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "60.00",
+    originalprice: "72.00",
+    name: "Women's adidas Space-Dye Polo Shirt",
+    gender: "Women",
+    size: "Large",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 16,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5242415_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "17.50",
+    originalprice: "25.00",
+    name: "https://media.kohlsimg.com/is/image/kohls/5242415_ALT?wid=240&hei=240&op_sharpen=1",
+    gender: "Women",
+    size: "xl",
+    brand: "Adidas",
+    category: "m Tops",
+    id: 17,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/4627432_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "50.00",
+    originalprice: "63.00",
+    name: "Women's adidas Linear French Terry Hoodie",
+    gender: "Women",
+    size: "Large",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 18,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/4935963_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "18.50",
+    originalprice: "35.00",
+    name: "Plus Size adidas U for U Long Sleeve Tee",
+    gender: "Women",
+    size: "Large",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 19,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5164220_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "22.00",
+    originalprice: "32.00",
+    name: "Plus Size adidas Performance Tee",
+    gender: "Women",
+    size: "xxl",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 20,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5010561_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "25.00",
+    originalprice: "35.00",
+    name: "Women's adidas Holiday Shine Graphic Tee",
+    gender: "Women",
+    size: "m",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 21,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5012705_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "27.00",
+    originalprice: "33.00",
+    name: "Women's adidas Essentials 3-Stripes Tee",
+    gender: "Women",
+    size: "s",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 22,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/4981894_Halo_Mint?wid=240&hei=240&op_sharpen=1",
+    saleprice: "55.00",
+    originalprice: "62.00",
+    name: "Women's adidas Space-Dye Sleeveless Polo Shirt",
+    gender: "Women",
+    size: "Large",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 23,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5324536?wid=240&hei=240&op_sharpen=1",
+    saleprice: "32.00",
+    originalprice: "39.00",
+    name: "Women's adidas 3 Stripe Crop Tee",
+    gender: "Women",
+    size: "xxl",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 24,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/5010513_ALT?wid=240&hei=240&op_sharpen=1",
+    saleprice: "17.50",
+    originalprice: "25.00",
+    name: "Women's adidas Foil Mandala Graphic Tee",
+    gender: "Women",
+    size: "s",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 25,
+  },
+  {
+    img: "https://media.kohlsimg.com/is/image/kohls/4656724_Black_White?wid=240&hei=240&op_sharpen=1",
+    saleprice: "12.50",
+    originalprice: "23.00",
+    name: "Women's adidas 3 Stripe Crop Tee",
+    gender: "Women",
+    size: "m",
+    brand: "Adidas",
+    category: "Women Tops",
+    id: 26,
+  },
+];
+
+localStorage.setItem("womensdata", JSON.stringify(womensdata));
+
+displayData(womensdata);
+
+function displayData(womensdata) {
+  document.querySelector(".items").innerHTML = "";
+
+  womensdata.map(function (ele, i, arr) {
+    var div = document.createElement("div");
+    div.setAttribute("class", "itm");
+
+    var image = document.createElement("img");
+    image.setAttribute("src", ele.img);
+
+    var saleprice = document.createElement("h3");
+    saleprice.setAttribute("class", "o_p");
+    saleprice.innerText = "$" + ele.saleprice;
+
+    var originalprice = document.createElement("p");
+    originalprice.innerText = "$" + ele.originalprice;
+    originalprice.setAttribute("class", "r_p");
+
+    var name = document.createElement("h5");
+    name.setAttribute("class", "name");
+    name.setAttribute("title", ele.name);
+    name.innerText = ele.name;
+
+    // var gender = document.createElement()
+
+    div.append(image, saleprice, originalprice, name);
+    document.querySelector(".items").append(div);
+    document.querySelector(".count").innerText = womensdata.length;
+  });
+}
+
+console.log(womensdata.length);
+
+function featuredData() {
+  var featured = document.getElementsByClassName("featured").value;
+  console.log(featured);
+  if (sorting == "featured") {
+    womensdata.sort(function (a, b) {
+      return b.originalprice - a.originalprice;
+    });
+    displayData(womensdata);
+  }
+}
+
+// function for sorting
+
+function sortItems() {
+  var sorting = document.getElementById("sort_item").value;
+  console.log(sorting);
+  if (sorting == "h2l") {
+    womensdata.sort(function (a, b) {
+      return b.saleprice - a.saleprice;
+    });
+    displayData(womensdata);
+  } else if (sorting == "l2h") {
+    womensdata.sort(function (a, b) {
+      return a.saleprice - b.saleprice;
+    });
+    displayData(womensdata);
+  } else if (sorting == "featured") {
+    womensdata.sort(function (a, b) {
+      return b.originalprice - a.originalprice;
+    });
+    displayData(womensdata);
+  }
+}
+
+// Function for filter
+
+// for xs
+var xs = document.querySelector(".xs");
+xs.addEventListener("click", function () {
+  var xsData = womensdata.filter(function (ele, i, array) {
+    return ele.size == "xs";
+  });
+  if (xs.checked) {
+    displayData(xsData);
+  } else {
+    displayData(womensdata);
+  }
+});
+
+// for s
+
+var s = document.querySelector(".s");
+s.addEventListener("click", function () {
+  var sData = womensdata.filter(function (ele, i, array) {
+    return ele.size == "s";
+  });
+  if (s.checked) {
+    displayData(sData);
+  } else {
+    displayData(womensdata);
+  }
+});
+
+// for m
+
+var m = document.querySelector(".m");
+m.addEventListener("click", function () {
+  var mData = womensdata.filter(function (ele, i, array) {
+    return ele.size == "m";
+  });
+  if (m.checked) {
+    displayData(mData);
+  } else {
+    displayData(womensdata);
+  }
+});
+
+// for large
+
+var l = document.querySelector(".l");
+l.addEventListener("click", function () {
+  var lData = womensdata.filter(function (ele, i, array) {
+    return ele.size == "Large";
+  });
+  if (l.checked) {
+    displayData(lData);
+  } else {
+    displayData(womensdata);
+  }
+});
+
+// for xl
+
+var xl = document.querySelector(".xl");
+xl.addEventListener("click", function () {
+  var xlData = womensdata.filter(function (ele, i, array) {
+    return ele.size == "xl";
+  });
+  if (xl.checked) {
+    displayData(xlData);
+  } else {
+    displayData(womensdata);
+  }
+});
+
+// for xxl
+
+var xxl = document.querySelector(".xxl");
+xxl.addEventListener("click", function () {
+  var xxlData = womensdata.filter(function (ele, i, array) {
+    return ele.size == "xxl";
+  });
+  if (xxl.checked) {
+    displayData(xxlData);
+  } else {
+    displayData(womensdata);
+  }
+});
+
+// function for search option
+
+function search() {
+  var searchText = document.querySelector(".input_search").value;
+  var filterData = womensdata.filter(function (ele) {
+    return ele.name.includes(searchText);
+  });
+  displayData(filterData);
+}
+
+// function for select by category
+
+// for shirt & tee's
+
+// var sh = document.querySelector(".shirt");
+// sh = addEventListener("click", function () {
+//   var shirt_Data = womensdata.filter(function (ele) {
+//     return ele.category == "s";
+//   });
+//   displayData(shirt_Data);
+// });
+
+// // for hoddies
+
+// var hoddie = document.querySelector(".hoddie");
+// hoddie = addEventListener("click", function () {
+//   var hoddie_Data = womensdata.filter(function (ele) {
+//     return ele.category == "h";
+//   });
+//   displayData(hoddie_Data);
+// });
+
+// //  for jeans
+
+// var jeans = document.querySelector(".jeans");
+// jeans = addEventListener("click", function () {
+//   var jeans_Data = womensdata.filter(function (ele) {
+//     return ele.category == "j";
+//   });
+//   displayData(jeans_Data);
+// });
+
+// //  for trousers
+
+// var trouser = document.querySelector(".trouser");
+// trouser = addEventListener("click", function () {
+//   var trouser_Data = womensdata.filter(function (ele) {
+//     return ele.category == "t";
+//   });
+//   displayData(trouser_Data);
+// });
+
+// //  for sportwears
+
+// var sports_wear = document.querySelector(".sport");
+// sports_wear = addEventListener("click", function () {
+//   var sports_wear_Data = womensdata.filter(function (ele) {
+//     return ele.category == "sw";
+//   });
+//   displayData(sports_wear_Data);
+// });
+
+// filter dropdown function for brand
+
+var li1 = document.querySelector(".brand");
+
+var li2 = document.querySelector(".sub_filter");
+
+li2.style.display = "none";
+
+li1.addEventListener("click", function () {
+  if (li2.style.display == "none") {
+    li2.style.display = "block";
+  } else {
+    li2.style.display = "none";
+  }
+});
+
+// filter dropdown function for size
+
+var li3 = document.querySelector(".size");
+
+var li4 = document.querySelector(".sub_filter1");
+
+li4.style.display = "none";
+
+li3.addEventListener("click", function () {
+  if (li4.style.display == "none") {
+    li4.style.display = "block";
+  } else {
+    li4.style.display = "none";
+  }
+});
